f95daed2a1a39242ca875736dcbba6639292d038
veddster <matthewvedder@gmail.com>
2016-10-19 13:25:09 -0400

    add axios call to rails api in Form component

--- app/components/Form.js
+++ app/components/Form.js
@@ -3,15 +3,25 @@ const Input = require('./Input');
 const { Form } = require('formsy-react');
 const InputContainer = require('../containers/InputContainer');
 const MainContainer = require('../containers/MainContainer');
+var axios = require ('axios');
 require('../styles.css');
 
+function handleSubmit (data) {
+  axios.post('http://localhost:3000/users', data) //'https://shielded-citadel-11180.herokuapp.com/users'
+    .then(function (response) {
+      alert(response.data.email_exists);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
 
 const myForm = React.createClass({
   getInitialState() {
     return { canSubmit: false };
   },
   submit(data) {
-    alert(JSON.stringify(data, null, 4));
+    handleSubmit(data);
   },
   enableButton() {
     this.setState({ canSubmit: true });
@@ -24,16 +34,16 @@ const myForm = React.createClass({
       <MainContainer title="Start Your Free Demo">
         <Form onSubmit={this.submit} onValid={this.enableButton} onInvalid={this.disableButton} >
           <InputContainer title="Name">
-            <Input value="" name="first-name" placeholder="First Name" type="text" required />
-            <Input value="" name="last-name" placeholder="Last Name" type="text" required />
+            <Input value="" name="first_name" placeholder="First Name" type="text" required />
+            <Input value="" name="last_name" placeholder="Last Name" type="text" required />
           </InputContainer>
           <InputContainer title="Account">
             <Input value="" name="email" placeholder="Email" validations="isEmail" validationError="Are you sure this is a valid email?" required />
             <Input value="" name="password" placeholder="Password" type="password" required />
           </InputContainer>
           <InputContainer title="Project Information">
-            <Input value="" name="project" placeholder="Project Name" type="text" required />
-            <Input value="" name="organization" placeholder="Organization Name" type="text" required />
+            <Input value="" name="project_name" placeholder="Project Name" type="text" required />
+            <Input value="" name="organization_name" placeholder="Organization Name" type="text" required />
           </InputContainer>
           <button className='btn btn-md btn-success button'type="submit" disabled={!this.state.canSubmit}>Start Your Demo</button>
         </Form>
