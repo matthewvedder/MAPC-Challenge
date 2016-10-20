b600e5aa4e17398c2bc1b2dc9c71e4cc7c673461
veddster <matthewvedder@gmail.com>
2016-10-19 20:03:04 -0400

    add functionality in Form component to display error message if email_exists is true

--- app/components/Form.js
+++ app/components/Form.js
@@ -1,27 +1,27 @@
 const React = require('react');
 const Input = require('./Input');
+const Link = require('react-router').Link;
 const { Form } = require('formsy-react');
 const InputContainer = require('../containers/InputContainer');
 const MainContainer = require('../containers/MainContainer');
-var axios = require ('axios');
+const axios = require ('axios');
 require('../styles.css');
 
-function handleSubmit (data) {
-  axios.post('http://localhost:3000/users', data) //'https://shielded-citadel-11180.herokuapp.com/users'
-    .then(function (response) {
-      alert(response.data.email_exists);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-}
 
+const link = 'here'.link('#')
 const myForm = React.createClass({
   getInitialState() {
     return { canSubmit: false };
   },
   submit(data) {
-    handleSubmit(data);
+    return axios.post('https://shielded-citadel-11180.herokuapp.com/users', data)
+      .then(function (response) {
+        if ( response.data.email_exists === true ) {
+          this.setState({ message: 'An account with the email ' + data.email + ' already exists! Please sign in.' });
+        } else {
+          this.setState({ message: null })
+        }
+    }.bind(this))
   },
   enableButton() {
     this.setState({ canSubmit: true });
@@ -33,6 +33,7 @@ const myForm = React.createClass({
     return (
       <MainContainer title="Start Your Free Demo">
         <Form onSubmit={this.submit} onValid={this.enableButton} onInvalid={this.disableButton} >
+          <p>{this.state.message}</p>
           <InputContainer title="Name">
             <Input value="" name="first_name" placeholder="First Name" type="text" required />
             <Input value="" name="last_name" placeholder="Last Name" type="text" required />
